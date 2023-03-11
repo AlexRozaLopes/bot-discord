b2fc9b41bf7b01a63848debdd758c4a58c658cbf
AlexRozaLopes <alexrozaestudos@gmail.com>
2023-03-11 20:05:34 -0300

    gitignore

--- src/main/java/org/example/ReadyListener.java
+++ src/main/java/org/example/ReadyListener.java
@@ -4,6 +4,7 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
+import org.example.utils.VariaveisSecretas;
 
 
 public class ReadyListener implements EventListener {
