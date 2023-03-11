d4493b84e1df80d2df501237d70177de26f5e76b
AlexRozaLopes <alexrozaestudos@gmail.com>
2023-03-11 20:21:26 -0300

    gitignore

--- src/main/java/org/example/MessageListener.java
+++ src/main/java/org/example/MessageListener.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.entities.channel.ChannelType;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.example.utils.VariaveisSecretas;
 
 public class MessageListener extends ListenerAdapter {
 
