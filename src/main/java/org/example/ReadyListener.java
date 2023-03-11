2ab8a9c257dd3f12366cde2b5ac23cd2017ece2b
AlexRozaLopes <alexrozaestudos@gmail.com>
2023-03-11 19:31:54 -0300

    criando as primeiras funcionalidades do bot!

---
+++ src/main/java/org/example/ReadyListener.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.hooks.EventListener;
+
+
+public class ReadyListener implements EventListener {
+
+    private final static String TOKEN = "MTA4NDIxNDQzNzQxMTU3Mzg0MA.GhT7qW.MBIyOCPWCmO5pExUoKT3UBKoRgWAdZJB2LwJC4";
+
+    public static void main(String[] args)
+            throws InterruptedException {
+        // Note: It is important to register your ReadyListener before building
+        var jda = JDABuilder.createDefault(TOKEN)
+                .addEventListeners(new ReadyListener())
+                .build();
+
+        // optionally block until JDA is ready
+        jda.awaitReady();
+    }
+
+    @Override
+    public void onEvent(GenericEvent event) {
+        if (event instanceof ReadyEvent)
+            System.out.println("API is ready!");
+    }
+}
+
