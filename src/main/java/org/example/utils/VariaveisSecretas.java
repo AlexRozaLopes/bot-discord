8ed3e9259b4d918840a11d391cc0a039ebdbb64d
AlexRozaLopes <alexrozaestudos@gmail.com>
2023-03-11 20:10:18 -0300

    gitignore

--- src/main/java/org/example/utils/VariaveisSecretas.java
+++
@@ -1,7 +0,0 @@
-package org.example.utils;
-
-public class VariaveisSecretas {
-
-    public static final String TOKEN = "MTA4NDIxNDQzNzQxMTU3Mzg0MA.GdIDId.FrbZIT9WOxtYsGPBjZkSTnzV3EjYP9lcZ-gtUc" ;
-
-}
