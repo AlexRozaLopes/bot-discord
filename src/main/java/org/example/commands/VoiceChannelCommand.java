64d5be1a0eee39b97d72da7534825cccff1e88b3
AlexRozaLopes <alexrozaestudos@gmail.com>
2023-03-12 12:12:45 -0300

    habilitando o poder do bilbo

--- src/main/java/org/example/commands/VoiceChannelCommand.java
+++ src/main/java/org/example/commands/VoiceChannelCommand.java
@@ -7,7 +7,12 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.managers.AudioManager;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class VoiceChannelCommand extends ListenerAdapter {
+
+    private ExecutorService executorService = Executors.newSingleThreadExecutor();
 
     private JDA jda;
 
@@ -20,27 +25,31 @@ public class VoiceChannelCommand extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
 
-        // Make sure we only respond to events that occur in a guild
-        if (!event.isFromGuild()) return;
-        // This makes sure we only execute our code when someone sends a message with "!play"
-        if (!event.getMessage().getContentRaw().startsWith("!play")) return;
-        // Now we want to exclude messages from bots since we want to avoid command loops in chat!
-        // this will include own messages as well for bot accounts
-        // if this is not a bot make sure to check if this message is sent by yourself!
-        if (event.getAuthor().isBot()) return;
-        Guild guild = event.getGuild();
-        // This will get the first voice channel with the name "music"
-        // matching by voiceChannel.getName().equalsIgnoreCase("music")
-        VoiceChannel channel = guild.getVoiceChannels().get(0);
-        AudioManager manager = guild.getAudioManager();
+        Runnable recordingTask = () -> {
+            // Make sure we only respond to events that occur in a guild
+            if (!event.isFromGuild()) return;
+            // This makes sure we only execute our code when someone sends a message with "!play"
+            if (!event.getMessage().getContentRaw().startsWith("!play")) return;
+            // Now we want to exclude messages from bots since we want to avoid command loops in chat!
+            // this will include own messages as well for bot accounts
+            // if this is not a bot make sure to check if this message is sent by yourself!
+            if (event.getAuthor().isBot()) return;
+            Guild guild = event.getGuild();
+            // This will get the first voice channel with the name "music"
+            // matching by voiceChannel.getName().equalsIgnoreCase("music")
+            VoiceChannel channel = guild.getVoiceChannels().get(0);
+            AudioManager manager = guild.getAudioManager();
 
-        // MySendHandler should be your AudioSendHandler implementation
-        recordCommand = new RecordCommand(jda);
-        manager.setSendingHandler(recordCommand);
-        // Here we finally connect to the target voice channel
-        // and it will automatically start pulling the audio from the MySendHandler instance
-        manager.openAudioConnection(channel);
-        recordCommand.startRecording(event);
+            // MySendHandler should be your AudioSendHandler implementation
+            recordCommand = new RecordCommand(jda);
+            manager.setSendingHandler(recordCommand);
+            // Here we finally connect to the target voice channel
+            // and it will automatically start pulling the audio from the MySendHandler instance
+            manager.openAudioConnection(channel);
+            recordCommand.startRecording(event);
+        };
+
+        executorService.submit(recordingTask);
 
     }
 }
